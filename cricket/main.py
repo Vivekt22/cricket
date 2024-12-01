9834ada310b39a7ec623c6747073bdcdbc17b410
vivekt22 <vivek-thomas@outlook.com>
2024-12-01 18:32:12 +0100

    Added static base catalog path

--- cricket/main.py
+++ cricket/main.py
@@ -9,6 +9,7 @@ from cricket.params import Params
 
 @flow(log_prints=True)
 def main_run(base_path:str):
+    base_path = "C:/Users/vivek/OneDrive/Documents/04 - My Projects/30 - Cricket/cricket"
     catalog = Catalog(base_path)
     params = Params()
 
